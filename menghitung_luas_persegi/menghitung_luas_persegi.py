05adfe461bfbc9ee3f3e00168cfc27414b6a0fb1
upii <luviaprilyansyahgabriel@gmail.com>
2025-06-07 23:17:54 +0800

    👨‍💻🐍

---
+++ menghitung_luas_persegi/menghitung_luas_persegi.py
@@ -0,0 +1,8 @@
+#menghitung luas persegi
+
+print("Menghitung luas persegi sederhana")
+print("="*20)
+sisi = float(input("Masukkan panjang sisi persegi: "))
+luas = sisi ** 2
+print("="*20)
+print(f'Luas persegi adalah {luas}')
